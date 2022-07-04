4f9194bf8d38f76697d428ca3bed87c1a551c63b
Chien Truong <tqc1234@gmail.com>
2022-07-04 03:21:23 -0400

    Added the questions for 6,7,8,9,10
    Priming these for solving some other time

--- euler6.py
+++ euler6.py
@@ -1,12 +1,15 @@
-"""The sum of the squares of the first ten natural numbers is,
+"""
+The sum of the squares of the first ten natural numbers is,
 1^2 + 2^2 +...+ 10^2 = 385
 The square of the sum of the first ten natural numbers is,
 (1 + 2 +...+ 10)^2 = 3025
 Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is .
 3025 - 385 = 2640
-Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum."""
+Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+"""
 
-""" We can shorten this a crap ton
+"""
+We can shorten this a crap ton
 Recall Binomial theorem
 Reference: https://planetmath.org/squareofsum
 
