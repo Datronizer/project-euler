49bce8d8da631a80c8b5a840e411cb8e1d4d2c4c
Chien Truong <tqc1234@gmail.com>
2022-07-11 19:34:30 -0400

    Updated Euler 7, 8
    9 is still in progress, Pythagorean squares are a bane of my existence

--- Problems 1-9/euler7.py
+++ Problems 1-9/euler7.py
@@ -3,3 +3,50 @@ By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that 
 
 What is the 10 001st prime number?
 """
+
+"""
+Method: Sieve of Eratosthenes
+
+For the method, refer to the sieve of Eratosthenes which is surprisingly good at
+finding primes up to a limit.
+
+Fortunately, the limit here is 10,001.
+"""
+import math
+
+limit = 10001
+counter = 1 # 2 is prime so we start at 1 rather than 0
+
+def isPrime(n):
+    if n == 1:
+        return False
+    elif n < 4: # take 2, 3 and move straight onto 4
+        return True
+    elif n % 2 == 0:
+        return False # skips 4, 6, 8
+    elif n < 9: # takes 5, 7
+        return True
+    elif n % 3 == 0:
+        return False
+    else:
+        # account for anything above 9. Recall that all primes > 3 can be written as 6k+1 or 6k-1.
+        # recall also that is prime if there exists no prime factors < sqrt(n)
+        r = math.sqrt(n) # r for real no., n for natural no.
+        f = 5 # the next prime on the sieve
+        while f <= r:
+            if n % f == 0: # this is for the 6k - 1
+                return False
+                break
+            elif n % (f+2) == 0: # this is for 6k + 1
+                return False
+                break
+            f += 6 # increment by 6 for the 6k loop
+        return True # if it passes both those cases then there are no factors meaning number is automatically prime
+
+number = 1
+while counter < limit:
+    number += 2 # we're adding 2 to skip all even numbers, we already accounted for 2 on line 18
+    if isPrime(number):
+        counter += 1
+
+print(number)
--- Problems 1-9/euler9.py
+++ Problems 1-9/euler9.py
@@ -1,9 +1,33 @@
 """
 A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
 
-a2 + b2 = c2
-For example, 32 + 42 = 9 + 16 = 25 = 52.
+a^2 + b^2 = c^2
+For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
 
 There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 Find the product abc.
 """
+
+"""
+This looks like another case of binomial theorem.
+We have:
+a^2 + b^2 = c^2
+a + b + c = 1000
+
+Isolate a and b to get:
+a^2 + b^2 = c^2
+a   + b   = 1000 - c
+
+Then:
+a^2 + b^2 + 2ab = c^2 + 2ab
+a^2 + b^2 + 2ab = (1000 - c)^2
+
+Thus
+> c^2 + 2ab = 1000^2 - 2000c + c^2
+> 2ab = 1000000 - 2000c
+> ab = 500000 - 1000c
+
+If c is 500 then ab = 0 and thus either of which will be 0 which isn't a natural number
+If c > 500 then ab is negative.
+Thus c must be < 500 and a < b < c < 500
+But if c < 500 and a + b + c = 1000 then a + b > 500
